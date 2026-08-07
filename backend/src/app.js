49dd1308de46c2c1cdf4c5b0e90b03f29db8ba1e
Afnan-coder <muhammadafnanali2@gmail.com>
2026-08-07 10:19:24 +0500

    feat(security): implement morgan middleware for loging the data in terminal

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import morgan from "morgan";
 
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(helmet());
+app.use(morgan("dev"));
 app.use(express.json());
 
 

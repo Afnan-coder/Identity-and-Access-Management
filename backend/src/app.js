68b420cf436d23e95ede17f54c15d020dba6db62
Afnan-coder <muhammadafnanali2@gmail.com>
2026-08-07 10:42:17 +0500

    feat(security): implement the express-rate-limit middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,22 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import ratelimit from "express-rate-limit"
 
 const app = express();
+
+const limiter = ratelimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100,
+    message: "Too many requests, please try again later."
+})
+
 
 //middleware
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+app.use(limiter);
 app.use(express.json());
 
 

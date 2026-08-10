c432b6727138b38f2784bd83b746e14cab141d44
Afnan-coder <muhammadafnanali2@gmail.com>
2026-08-10 10:27:54 +0500

    feat(database): create organization schema

---
+++ backend/src/models/Organization.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const organizationSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        description: {
+            type: String,
+            trim: true,
+        },
+
+        status: {
+            type: String,
+            enum: ["active", "inactive"],
+            default: "active",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Organization = mongoose.model(
+    "Organization",
+    organizationSchema
+);
+
+export default Organization;
